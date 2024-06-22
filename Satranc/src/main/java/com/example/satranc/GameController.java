0f8a8c03b828d0d911fae6251ec33ccd6797100f
Caducues <222805014@ogr.cbu.edu.tr>
2024-06-22 11:43:41 +0300

    Based Interface

---
+++ Satranc/src/main/java/com/example/satranc/GameController.java
@@ -0,0 +1,298 @@
+package com.example.satranc;
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.input.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Rectangle;
+import java.util.logging.Handler;
+
+public class GameController {
+
+    @FXML
+    private ImageView bishop;
+
+    @FXML
+    private ImageView bishop2;
+
+    @FXML
+    private ImageView king;
+
+    @FXML
+    private ImageView knight;
+
+    @FXML
+    private ImageView knight2;
+
+    @FXML
+    private ImageView pawn;
+
+    @FXML
+    private ImageView pawn2;
+
+    @FXML
+    private ImageView pawn3;
+
+    @FXML
+    private ImageView pawn4;
+
+    @FXML
+    private ImageView pawn5;
+
+    @FXML
+    private ImageView pawn6;
+
+    @FXML
+    private ImageView pawn7;
+
+    @FXML
+    private ImageView pawn8;
+
+    @FXML
+    private ImageView queen;
+
+    @FXML
+    private AnchorPane r0c0;
+
+    @FXML
+    private AnchorPane r0c1;
+
+    @FXML
+    private AnchorPane r0c2;
+
+    @FXML
+    private AnchorPane r0c3;
+
+    @FXML
+    private AnchorPane r0c4;
+
+    @FXML
+    private AnchorPane r0c5;
+
+    @FXML
+    private AnchorPane r0c6;
+
+    @FXML
+    private AnchorPane r0c7;
+
+    @FXML
+    private AnchorPane r1c0;
+
+    @FXML
+    private AnchorPane r1c1;
+
+    @FXML
+    private AnchorPane r1c2;
+
+    @FXML
+    private AnchorPane r1c3;
+
+    @FXML
+    private AnchorPane r1c4;
+
+    @FXML
+    private AnchorPane r1c5;
+
+    @FXML
+    private AnchorPane r1c6;
+
+    @FXML
+    private AnchorPane r1c7;
+
+    @FXML
+    private AnchorPane r2c0;
+
+    @FXML
+    private AnchorPane r2c1;
+
+    @FXML
+    private AnchorPane r2c2;
+
+    @FXML
+    private AnchorPane r2c3;
+
+    @FXML
+    private AnchorPane r2c4;
+
+    @FXML
+    private AnchorPane r2c5;
+
+    @FXML
+    private AnchorPane r2c6;
+
+    @FXML
+    private AnchorPane r2c7;
+
+    @FXML
+    private AnchorPane r3c1;
+
+    @FXML
+    private AnchorPane r3c2;
+
+    @FXML
+    private AnchorPane r3c3;
+
+    @FXML
+    private AnchorPane r3c4;
+
+    @FXML
+    private AnchorPane r3c5;
+
+    @FXML
+    private AnchorPane r3c6;
+
+    @FXML
+    private AnchorPane r3c7;
+
+    @FXML
+    private AnchorPane r4c0;
+
+    @FXML
+    private AnchorPane r4c1;
+
+    @FXML
+    private AnchorPane r4c2;
+
+    @FXML
+    private AnchorPane r4c3;
+
+    @FXML
+    private AnchorPane r4c4;
+
+    @FXML
+    private AnchorPane r4c5;
+
+    @FXML
+    private AnchorPane r4c6;
+
+    @FXML
+    private AnchorPane r4c7;
+
+    @FXML
+    private AnchorPane r5c0;
+
+    @FXML
+    private AnchorPane r5c1;
+
+    @FXML
+    private AnchorPane r5c2;
+
+    @FXML
+    private AnchorPane r5c3;
+
+    @FXML
+    private AnchorPane r5c4;
+
+    @FXML
+    private AnchorPane r5c5;
+
+    @FXML
+    private AnchorPane r5c6;
+
+    @FXML
+    private AnchorPane r5c7;
+
+    @FXML
+    private AnchorPane r6c0;
+
+    @FXML
+    private AnchorPane r6c1;
+
+    @FXML
+    private AnchorPane r6c2;
+
+    @FXML
+    private AnchorPane r6c3;
+
+    @FXML
+    private AnchorPane r6c4;
+
+    @FXML
+    private AnchorPane r6c5;
+
+    @FXML
+    private AnchorPane r6c6;
+
+    @FXML
+    private AnchorPane r6c7;
+
+    @FXML
+    private AnchorPane r7c0;
+
+    @FXML
+    private AnchorPane r7c1;
+
+    @FXML
+    private AnchorPane r7c2;
+
+    @FXML
+    private AnchorPane r7c3;
+
+    @FXML
+    private AnchorPane r7c4;
+
+    @FXML
+    private AnchorPane r7c5;
+
+    @FXML
+    private AnchorPane r7c6;
+
+    @FXML
+    private AnchorPane r7c7;
+
+    @FXML
+    private ImageView rook1;
+
+    @FXML
+    private ImageView rook2;
+
+    @FXML
+    private ImageView wbishop;
+
+    @FXML
+    private ImageView wbishop2;
+
+    @FXML
+    private ImageView wking;
+
+    @FXML
+    private ImageView wknight;
+
+    @FXML
+    private ImageView wpawn;
+
+    @FXML
+    private ImageView wpawn2;
+
+    @FXML
+    private ImageView wpawn3;
+
+    @FXML
+    private ImageView wpawn4;
+
+    @FXML
+    private ImageView wpawn5;
+
+    @FXML
+    private ImageView wpawn6;
+
+    @FXML
+    private ImageView wpawn7;
+
+    @FXML
+    private ImageView wpawn8;
+
+    @FXML
+    private ImageView wqueen;
+
+    @FXML
+    private ImageView wrook;
+
+}
